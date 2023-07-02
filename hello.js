78e26565d9e756a23fab783c6d009f7c917165a7
MuraliMuraliP <murali.p@pyramidions.in>
2023-07-01 17:07:19 -0700

    wel

---
+++ hello.js
@@ -0,0 +1,13 @@
+// To declare the array
+  let myArray = [1, 2, 3, 4, 5];
+  document.write("myArray = " + myArray + "<br>");
+  let fruits = ["Apple", "Banana", "Kiwi"]
+
+  // To get the first value of the array
+  document.write("First item of Fruits Array = " + fruits[0] + "<br>");
+
+  // To Push & Pop the array
+  fruits.push("Orange"); // adds 6 to the end of the array
+  document.write("Fruits Array  = " + fruits + "<br>");
+  fruits.pop(); // removes the last element (Orange) from the array
+  document.write("Fruits Array after Pop  = " + fruits + "<br>");
